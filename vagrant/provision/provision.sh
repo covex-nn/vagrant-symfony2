c16aa8cc86ccd310be782a23a2c9ea5891b68b1d
Andrey F. Mindubaev <covex.mobile@gmail.com>
2014-05-21 20:06:39 +0400

    Fix samba config

--- vagrant/provision/provision.sh
+++ vagrant/provision/provision.sh
@@ -176,6 +176,6 @@ mkdir /home/vagrant/share
 chown vagrant:vagrant /home/vagrant/share
 cp /srv/config/smb.conf /etc/samba/smb-vagrant-share.conf
 chmod 644 /etc/samba/smb-vagrant-share.conf
-echo "[vagrant]" >> /etc/samba/smb.conf
+echo "[public]" >> /etc/samba/smb.conf
 echo "include = /etc/samba/smb-vagrant-share.conf" >> /etc/samba/smb.conf
 service smbd restart
